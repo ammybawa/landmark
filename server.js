5335ed3e77fc88064008d8fd48cfa5bc91f1f989
ammybawa <169134119+ammybawa@users.noreply.github.com>
2025-09-15 18:33:41 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+ 
+// Serve static files from build
+app.use(express.static(path.join(__dirname, 'build')));
+ 
+// Serve index.html for all other routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+ 
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
